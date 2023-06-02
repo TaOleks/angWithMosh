1db80ac3ece91e99e13b1f7daf5927ca24fc1060
TaOleks <tahtay.aleksey@gmail.com>
2023-06-02 16:21:57 +0200

    Building Re-usable Component
    Aliasing input Properties

---
+++ src/app/favorite/favorite.component.ts
@@ -0,0 +1,18 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-favorite',
+  templateUrl: './favorite.component.html',
+  styleUrls: ['./favorite.component.css']
+})
+export class FavoriteComponent {
+  @Input('isFavorite') isSelected: boolean=false;
+
+  onClick() {
+    this.isSelected = !this.isSelected;
+
+  }
+}
+
+
+
