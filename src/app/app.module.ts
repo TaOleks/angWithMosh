56c48e8800a660144458e41e24ab1c1d1e2cd32b
TaOleks <tahtay.aleksey@gmail.com>
2023-06-04 18:51:05 +0200

    ng-content
    we don't need a selector when we use only one ng-content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FavoriteComponent } from './favorite/favorite.component';
+import { BootstrapPanelComponent } from './bootstrap-panel/bootstrap-panel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FavoriteComponent
+    FavoriteComponent,
+    BootstrapPanelComponent
   ],
   imports: [
     BrowserModule
