e601dcdafda9d39db08b3f5bb94fcbefea83c1f4
TaOleks <tahtay.aleksey@gmail.com>
2023-06-08 11:25:28 +0200

    ngFor
    (use- index:nunber; count: number; first:boolean; last:boolean;
    even:boolean; odd:boolen )

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,9 @@ import { FavoriteChangedEventArgs } from './favorite/favorite.component';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-viewMode = 'map';
+courses=[
+  {id:1, name:'course 1'},
+  {id:2, name:'course 2'},
+  {id:3, name:'course 3'},
+]
 }
