a66ce885655d5663271e0ac4fbd83d502454050c
TaOleks <tahtay.aleksey@gmail.com>
2023-06-08 17:00:15 +0200

    ngFor and change detections

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,4 +13,18 @@ courses=[
   {id:2, name:'course 2'},
   {id:3, name:'course 3'},
 ]
+
+onAdd(){
+  this.courses.push({id:4, name:'course 4'})
+}
+
+onRemove(course:{id:number,name:string}):void{
+  let index= this.courses.indexOf(course)
+  this.courses.splice(index, 1)
+//  this.courses = this.courses.filter(it=>it!=course) -  for me it's better
+}
+
+onChange(course:{id:number,name:string}){
+  course.name = 'UPDATE'
+}
 }
