251d074b6e83071ecd56b226ae54e5f1e97f074c
TaOleks <tahtay.aleksey@gmail.com>
2023-07-13 17:50:36 +0200

    Reactive Forms. Implementing Custom Validation

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { UsernameValidators } from './username.validators';
 
 @Component({
   selector: 'app-signup-form',
@@ -12,7 +13,9 @@ export class SignUpFormComponent {
 
     username:new FormControl('', [
     Validators.required,
-    Validators.minLength(3)]),
+    Validators.minLength(3),
+    UsernameValidators.cannotContainSpace
+  ]),
     password: new FormControl('', Validators.required)
   })
 
