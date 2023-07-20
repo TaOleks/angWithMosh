c37094802f0d5b07ff70f0810dcee63d935138d5
TaOleks <tahtay.aleksey@gmail.com>
2023-07-20 17:31:44 +0200

    Consuming HTTP Services . Creating DATA

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -7,11 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./posts.component.css']
 })
 export class PostsComponent {
-  posts:any
-  constructor(http:HttpClient){
-    http.get('https://jsonplaceholder.typicode.com/posts')
+  posts:any;
+  private url='https://jsonplaceholder.typicode.com/posts'
+  constructor(private http:HttpClient){
+    http.get(this.url)
     .subscribe(response =>{
      this.posts=response
     })
   }
+createPost(input:HTMLInputElement){
+  let post:any={title:input.value}
+  input.value='';
+  // always use JSON.syringify()method to convert object into a string
+  // SENDING DATA to a web server must to be a STRING
+this.http.post(this.url,JSON.stringify(post))
+.subscribe(response =>{
+  post.id=response
+  this.posts.splice(0,0,post)
+console.log (this.posts)
+})
+}
+
 }
