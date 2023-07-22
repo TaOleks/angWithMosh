7324d96db4cb2141b3e1446117c97ff87950e1a4
TaOleks <tahtay.aleksey@gmail.com>
2023-07-22 06:43:51 +0200

    OnInit Interface

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,19 +1,16 @@
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
-export class PostsComponent {
+export class PostsComponent implements OnInit {
   posts:any;
   private url='https://jsonplaceholder.typicode.com/posts'
   constructor(private http:HttpClient){
-    http.get(this.url)
-    .subscribe(response =>{
-     this.posts=response
-    })
+
   }
 createPost(input:HTMLInputElement){
   let post:any={title:input.value}
@@ -26,6 +23,13 @@ this.http.post(this.url,JSON.stringify(post))
   this.posts.splice(0,0,post)
 console.log (this.posts)
 })
+}
+
+ngOnInit(): void {
+this.http.get(this.url)
+    .subscribe(response =>{
+     this.posts=response
+    })
 }
 
 updatePost(post:any){
